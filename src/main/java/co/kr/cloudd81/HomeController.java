0143be66a8d051eceb647a5cd720d555cad05175
cloudd81 <kijurse@naver.com>
2022-11-16 18:10:25 +0900

    HomeController

---
+++ src/main/java/co/kr/cloudd81/HomeController.java
@@ -0,0 +1,22 @@
+package co.kr.cloudd81;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	public HomeController() {
+		System.out.println("------------HomeController() 객체 생성됨");
+	} // end
+	
+	@RequestMapping("/")
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		// redirect : 등록한 명령어를 호출할 수 있다
+		mav.setViewName("/index");
+		return mav;		
+	} // home() end
+	
+} // class end
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>index.jsp</title>
+</head>
+<body>
+	<h3>인덱스 페이지</h3>
+</body>
+</html>
